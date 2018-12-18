ae08747de0ea24be976551d33121517a486d19e0
ArPiT <arpit2438735@gmail.com>
2018-12-19 00:29:26 +0530

    Added TDJSON framework

---
+++ src/platforms/ios/TDJSON.framework/Headers/TDJSON.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for TDJSON.
+FOUNDATION_EXPORT double TDJSONVersionNumber;
+
+//! Project version string for TDJSON.
+FOUNDATION_EXPORT const unsigned char TDJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDJSON/PublicHeader.h>
+
+
